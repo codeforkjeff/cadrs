c31e17e733b009f1ea3a29e2cda0406fb9ac7160
Jeff Chiu <jchiu@ccedresults.org>
2019-09-19 15:42:16 -0700

    revert my changes to files I don't understand

--- R/post_hs_preprocess.R
+++ R/post_hs_preprocess.R
@@ -5,7 +5,7 @@ library(tidyverse)
 nsc_fn <- "/home/joseh/data/cadr_update/postSecDems.txt"
 nsc <- fread(nsc_fn, na.strings = c("NA", "NULL"))
 
-enr_fn <- "~/data/cadrs/enrollments.tx"
+enr_fn <- "~/data/cadrs/enrollments.txt"
 enr <- read_delim(enr_fn, delim = "|", col_names = TRUE, na = c("", "NA", "NULL")) 
 # Get HS grads
 names(enr)
